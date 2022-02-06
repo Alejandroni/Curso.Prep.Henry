889724c90dacdff6da9c6ba46c0e13ac94881e4e
Alejandroni <betanmendoza@gmail.com>
2022-02-06 01:20:17 -0500

    nuevos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -29,6 +29,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status === 1){
+    return "Online"
+  } else if(status === 2){
+return "Away"
+  }else{
+    "Offline"
+  } 
 }
 
 function saludo(idioma) {
@@ -38,6 +45,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === 'aleman'){
+    return "Guten Tag!"
+  } else if(idioma === 'mandarin'){
+    "Ni Hao!"
+  }else if(idioma === 'ingles'){
+    "Hello!"
+  } else {
+    "Hola!"
+  }
 }
 
 function colors(color) {
